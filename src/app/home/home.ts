b9beacfd644f0d0c376b993c7d4c5818727f460e
FerJustinianoAyllon <fernandajustinianoayllon@gmail.com>
2025-09-22 20:09:10 -0400

    feat(noti): Permiso de notificación local y botones de demostración

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -2,10 +2,39 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  imports: [],
   templateUrl: './home.html',
-  styleUrl: './home.scss'
+  styleUrls: ['./home.scss']
 })
 export class Home {
 
+ // Pedir permiso para notificaciones
+  requestPermission() {
+    if (!('Notification' in window)) {
+      alert('Este navegador no soporta notificaciones.');
+      return;
+    }
+
+    Notification.requestPermission().then(permission => {
+      console.log('Permiso:', permission); // 👈 revisa en la consola
+      if (permission === 'granted') {
+        alert('✅ Permiso concedido para notificaciones.');
+      } else {
+        alert('❌ Permiso denegado para notificaciones.');
+      }
+    });
+  }
+
+  // Probar notificación
+  showNotification() {
+    console.log('Estado actual:', Notification.permission); // 👈 revisa en consola
+
+    if (Notification.permission === 'granted') {
+      new Notification('¡Hola!', {
+        body: 'Esta es una notificación de prueba desde Angular 🚀',
+        icon: 'assets/icon.png' // opcional
+      });
+    } else {
+      alert('Primero debes permitir las notificaciones.');
+    }
+  }
 }
